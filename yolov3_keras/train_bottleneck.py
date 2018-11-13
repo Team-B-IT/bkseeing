83289c043224dbc82ce765736eb6f7877b62da18
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-11-14 00:12:27 +0700

    Update yolov3_keras/train_bottleneck.py

--- yolov3_keras/train_bottleneck.py
+++ yolov3_keras/train_bottleneck.py
@@ -76,7 +76,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-3), loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
-        batch_size = 8
+        batch_size = 4
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
