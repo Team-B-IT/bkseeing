5e928f761a9b3eec5197fbc4fc97b3d5b20aba7a
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-11-13 19:40:38 +0700

    update convert_annotation.py and train.txt

--- yolov3_keras/convert_annotation.py
+++ yolov3_keras/convert_annotation.py
@@ -31,9 +31,9 @@ def convert_annotation(image_id, list_file):
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
         b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
-        if (b[0] not in range(0,w+1) or b[1] not in range(0,w+1)):
+        if (b[0] not in range(1,w) or b[1] not in range(1,w)):
             continue
-        if (b[1] not in range(0,h+1) or b[3] not in range(0,h+1)):
+        if (b[1] not in range(1,h) or b[3] not in range(1,h)):
             continue
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
