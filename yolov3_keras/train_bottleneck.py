042ab1e8c7c2dfbe546793c06c36318b042480b8
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-11-17 15:08:44 +0700

    update something

--- yolov3_keras/train_bottleneck.py
+++ yolov3_keras/train_bottleneck.py
@@ -101,8 +101,8 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=40,
-            initial_epoch=30,
+            epochs=55,
+            initial_epoch=40,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
         model.save_weights(log_dir + 'trained_weights_prefinal_1.h5')
 
