d3c451306224770e7e5ecc74a7df5ce9b9cfc6a1
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2018-11-22 01:11:17 +0700

    Update train.py

--- yolov3_keras/train.py
+++ yolov3_keras/train.py
@@ -31,7 +31,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/3.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/4.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -80,10 +80,10 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=30,
-            initial_epoch=25,
+            epochs=40,
+            initial_epoch=30,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights('model_data/4.h5')
+        model.save_weights('model_data/5.h5')
 
     # Further training if needed.
 
