1430ab886486e6acbb1732eef799116456d25097
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-01-31 18:02:51 +0700

    Update train.py

--- Model/train.py
+++ Model/train.py
@@ -31,7 +31,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/frozen001.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -61,10 +61,10 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=20,
-                initial_epoch=0,
+                epochs=50,
+                initial_epoch=20,
                 callbacks=[logging, checkpoint])
-        model.save_weights('model_data/frozen001.h5')
+        model.save_weights('model_data/frozen002.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
