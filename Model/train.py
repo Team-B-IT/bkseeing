902af1a5b056b704f1ffa0ebd4e8a46b7182d43f
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-02-02 20:09:53 +0700

    Update train.py

--- Model/train.py
+++ Model/train.py
@@ -32,7 +32,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/frozen_1.0.0_data_version_2.0.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/frozen_1.0.1_data_version_2.0.h5') # make sure you know what you freeze
         # plot_model(model, to_file='model.png', show_shapes=True)
 
     logging = TensorBoard(log_dir=log_dir)
@@ -52,8 +52,8 @@ def _main():
 
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
-    if True:
-        model.compile(optimizer=Adam(lr=1e-5), loss={
+    if False:
+        model.compile(optimizer=Adam(lr=1e-3), loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
@@ -70,7 +70,7 @@ def _main():
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
-    if False:
+    if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
         model.compile(optimizer=Adam(lr=1e-7), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
@@ -82,10 +82,10 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=80,
+            epochs=60,
+            initial_epoch=40,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights('model_data/finetune001.h5')
+        model.save_weights('model_data/finetune_1.0.0_data_version_2.0.h5')
 
     # Further training if needed.
 
