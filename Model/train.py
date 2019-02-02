c87e818b26bb794518330c18fa71233c3f392ac6
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-02-02 11:56:11 +0700

    Update train.py

--- Model/train.py
+++ Model/train.py
@@ -32,7 +32,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/frozen_1.0.0_data_version_2.0.h5') # make sure you know what you freeze
         # plot_model(model, to_file='model.png', show_shapes=True)
 
     logging = TensorBoard(log_dir=log_dir)
@@ -53,7 +53,7 @@ def _main():
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
     if True:
-        model.compile(optimizer=Adam(lr=1e-3), loss={
+        model.compile(optimizer=Adam(lr=1e-5), loss={
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
@@ -63,10 +63,10 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=20,
-                initial_epoch=0,
+                epochs=40,
+                initial_epoch=20,
                 callbacks=[logging, checkpoint])
-        model.save_weights('model_data/frozen_1.0.0_data_version_2.0.h5')
+        model.save_weights('model_data/frozen_1.0.1_data_version_2.0.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
